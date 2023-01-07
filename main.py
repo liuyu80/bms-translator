0344d1c351bbb5623220669ea3de2561d029cf87
liuyu <2543722345@qq.com>
2023-01-07 16:24:53 +0800

    feat: 识别多帧报文名称
    能识别开始、回应、结束和数据帧

--- main.py
+++ main.py
@@ -7,13 +7,24 @@
  Author       : liuyu
  Date         : 2023-01-07 09:04:55
  LastEditors  : liuyu
- LastEditTime : 2023-01-07 15:18:23
+ LastEditTime : 2023-01-07 16:17:16
  FilePath     : \\BMS-translator\\main.py
  Copyright (C) 2023 liuyu. All rights reserved.
 '''
 
 import json, os, csv, struct, time
 import pandas as pd
+
+
+more_frame_config = {
+    'name': '',
+    'reply': False,
+    'start': False,
+    'end': False,
+    'total': 0,
+    'count': 0,
+    'total_bytes': 0
+}
 
 def read_json(path = 'bmsConfig·.json'):
     with open(path, "r", encoding='utf-8') as fp:
@@ -35,30 +46,60 @@ def get_csv_data(path):
     csv_df['时间标识'] = create_time + csv_df['时间标识']
     # print(csv_df.head())
     return csv_df
+    
+def set_more_frame_name(pgn, bit, dataRaw, priority):
+    if bit == 0x10:
+        more_frame_config['start'] = True
+        more_frame_config['end'] = False
+        more_frame_config['reply'] = False
+    elif bit == 0x11:
+        more_frame_config['total'] = dataRaw & 0xff000000000000
+        more_frame_config['reply'] = True
+        more_frame_config['start'] = False
+    elif bit == 0x13:
+        more_frame_config['end'] = True
+        more_frame_config['start'] = False
+        more_frame_config['reply'] = False
+        more_frame_config['total'] = 0
+        more_frame_config['count'] = 0
+        more_frame_config['name'] = ''
+        more_frame_config['total_bytes'] = 0
 
-def more_frame_deal(id, priority, receive_send, dataRaw):
-    pgn = dataRaw & 0xffff
+
     for key in data_js.keys():
+        if int(data_js[key]['PGN'], 16) == pgn and \
+        data_js[key]['priority'] == priority:
+            more_frame_config['name'] = key
+            if more_frame_config['start']:
+                return key + '-start'
+            elif more_frame_config['reply']:
+                return key + '-reply'
+            elif more_frame_config['end']:
+                more_frame_config['end'] = False
+                return key + '-end'
+    return '非标未识别'
+            
 
-        pass
 
 def find_bms_name(id, pgn, priority, receive_send, dataLength, dataRaw):
-
+    if pgn == 0xec00: 
+        pgn = dataRaw & 0xffff
+        control_bit = dataRaw >> 4*14
+        return set_more_frame_name(pgn, control_bit, dataRaw, priority)
+        
     for key in data_js.keys():
         if int(data_js[key]['PGN'], 16) == pgn and \
         data_js[key]['priority'] == priority and \
         int(data_js[key]['receive_send'], 16) == receive_send:
-            print(key)
             return key
-        if pgn == 0xec00: 
-            print(more_frame_deal(id, priority, receive_send, dataRaw))
-            return '多帧-应答'
         if pgn == 0xeb00:
-            return '多帧-数据'
+            if more_frame_config['name']:
+                more_frame_config['count'] += 1
+                return f"{more_frame_config['name']}-{more_frame_config['count']}"
+
     return '非标未识别'
 
 def param_msg_name(data):
-
     id = int(data['帧ID'], 16)               #帧ID
     dataLength = data['数据长度']                #数据长度
     dataRaw = int(data['数据(HEX)'].replace(' ',''), 16)      #数据(HEX)
@@ -70,7 +111,7 @@ def param_msg_name(data):
 
 
 def set_msg_name(df):
-    df['名称'] = df.loc[ :, ['序号', '帧ID', '数据长度', '数据(HEX)']].apply(param_msg_name, axis=1)
+    df['名称'] = df.loc[ : , ['序号', '帧ID', '数据长度', '数据(HEX)']].apply(param_msg_name, axis=1)
     return df
 
 
