13805a6867a7008900db9a2cd1436f5caba31bef
liuyu <2543722345@qq.com>
2023-01-30 17:49:44 +0800

    test

--- test/1.py
+++ test/1.py
@@ -1,20 +1,32 @@
-import struct, bitstring
+import struct
 
-data = '04 00 A0 0F 00 00 FD'
-data = int(data.replace(' ','')[:7*2], 16).to_bytes(7, byteorder="big", signed=False)
+data = 'FD'
+
+data = int(data.replace(' ',''), 16).to_bytes(int(len(data)/2), byteorder="big", signed=False)
 print(data)
 # print(struct.unpack('3s', data))
-pack = (struct.unpack('1s 1s 1s 1s 1s 1s 1s', data))
-for cell in pack:
-    print(int.from_bytes(cell, 'little'), '  ')
-
-print(pack)
-# # data = int(data.replace(' ',''), 16)
-# b = '0'*7 + bin(int.from_bytes(pack[2], 'little'))[2:]
-# print(b, type(b))
-# b = b
-# print(b,int(b[-12:], 2))
-# print(int(b[12:], 2))
+pack = list(struct.unpack('1s', data))
+for num in range(len(pack)):
+    pack[num] = int.from_bytes(pack[num], 'little')
 
 
-# pack = struct.pack('')
+def cut(obj, sec):
+    return [obj[i:i+sec] for i in range(0,len(obj),sec)]
+
+
+range_list = [6.7, 0.2]
+range_list = [int(str(range_list[0])[-1])-1, int(str(range_list[1])[-1])+ int(str(range_list[0])[-1])-1]
+
+def bit_overturn(obj) -> str:
+    obj = bin(obj)
+    s_list = [(obj)[i:i+2] for i in range(2,len(obj),2)]
+    s_str =  ''.join(s_list[::-1])
+    if s_str == '0':
+        return '0'* 8
+    if len(s_str) < 8:
+        return s_str + '0'*(8-len(s_str))
+    if len(s_str) == 8:
+        return s_str
+s_str = bit_overturn(6)
+
+print(s_str, type(0))
